a65a5d32c128bd22bf2c6b0236915d27acfbb182
hbollon <hugo.bollon@gmail.com>
2022-04-15 16:50:43 +0200

    feat: helper func to handle double escaped characters

--- internal/format/format.go
+++ internal/format/format.go
@@ -1,9 +1,35 @@
 package format
 
+import "errors"
+
 func DepthAlign(depth int) string {
 	var output string
 	for i := 0; i < depth; i++ {
 		output += "\t"
 	}
 	return output
+}
+
+func HandleDoubleEscapedCharacter(c rune) (rune, error) {
+	switch c {
+	case 'b':
+		return '\b', nil
+	case 'f':
+		return '\f', nil
+	case 't':
+		return '\t', nil
+	case 'n':
+		return '\n', nil
+	case 'r':
+		return '\r', nil
+	case '"':
+	case '\'':
+	case '\\':
+	case '/':
+		return c, nil
+	default:
+		return 0, errors.New("HandleDoubleEscapedCharacter: Unsupported character")
+	}
+
+	return 0, nil
 }
