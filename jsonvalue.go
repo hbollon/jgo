b5295c1b4ab75c84cd243802f8ad1bd87d0760e1
hbollon <hugo.bollon@gmail.com>
2022-04-15 16:52:49 +0200

    feat(jsonvalue): generic instanciation funcs

--- jsonvalue.go
+++ jsonvalue.go
@@ -4,14 +4,53 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 type JSONValue struct {
 	JSONValueType
 }
 
-func (obj *JSONValue[T]) Set(value T) {
-	obj.Value.toString()
+func CreateJSONValue(value any) JSONValue {
+	if value == nil {
+		return JSONValue{}
+	}
+
+	switch value.(type) {
+	case string:
+		return JSONValue{&stringType{value.(string)}}
+	case float32:
+		return JSONValue{&floatType{value.(float64)}}
+	case int:
+		return JSONValue{&integerType{value.(int)}}
+	case bool:
+		return JSONValue{&boolType{value.(bool)}}
+	default:
+		return JSONValue{}
+	}
+}
+
+func CreateJSONValueFromString(value string) JSONValue {
+	if value == "" {
+		return JSONValue{}
+	}
+
+	switch value {
+	case "true":
+		return JSONValue{&boolType{true}}
+	case "false":
+		return JSONValue{&boolType{false}}
+	case "null":
+		return JSONValue{}
+	}
+
+	if val, err := strconv.ParseFloat(value, 32); err == nil {
+		return JSONValue{&floatType{val}}
+	} else if val, err := strconv.ParseInt(value, 10, 32); err == nil {
+		return JSONValue{&integerType{int(val)}}
+	} else {
+		return JSONValue{&stringType{value}}
+	}
 }
 
 func (obj *JSONValue) Set(value JSONValueType) error {
